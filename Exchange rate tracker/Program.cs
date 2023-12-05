747419c8125ca8a4a9a259a29a16383b0fdb5fe3
DESKTOP-9FT8NSD\THERE WAS PIPIKO <lukakashia10@gmail.com>
2023-12-05 06:08:09 -0800

    Exchange

---
+++ Exchange rate tracker/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        while (true)
+        {
+            await FetchAndDisplayExchangeRates();
+
+            await Task.Delay(TimeSpan.FromMinutes(5));
+        }
+    }
+
+    public static async Task FetchAndDisplayExchangeRates()
+    {
+        try
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string url = "https://v6.exchangerate-api.com/v6/74c1bb7f4ebe559ae6f72796/latest/USD";
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string responseContent = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Exchange Rate Data:");
+                Console.WriteLine(responseContent);
+            }
+        }
+        catch (HttpRequestException ex)
+        {
+            Console.WriteLine("Failed to fetch exchange rates. Error: " + ex.Message);
+        }
+    }
+}
